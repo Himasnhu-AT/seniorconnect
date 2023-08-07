f4cd803da65708514a739fefb9efa9de6cef56e3
Himasnhu-AT <aristech.official@gmail.com>
2023-08-08 02:58:21 +0530

    middle commit 1

---
+++ lib/constants/widget/navigationbarhomescreen.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:seniorconnect/screen/homescreens/homescreen.dart';
+import 'package:seniorconnect/screen/login/login.dart';
+
+import '../Global_Variables.dart';
+// import 'package:badges/badges.dart';
+
+class BottomBar extends StatefulWidget {
+  static const String routeName = '/actual-home';
+  const BottomBar({super.key});
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int _page = 0;
+  double bottomBarWidth = 42;
+  double bottomBarBorderWidth = 5;
+
+  List<Widget> pages = [
+    const HomeScreen(),
+    const Loginpage(),
+    const Center(
+      child: Text('Cart Page'),
+    ),
+  ];
+
+  void updatePage(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[_page],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _page,
+        selectedItemColor: GlobalVariable.selectedNavBarColor,
+        unselectedItemColor: GlobalVariable.unselectedNavBarColor,
+        backgroundColor: GlobalVariable.backgroundColor,
+        iconSize: 28,
+        onTap: updatePage,
+        items: [
+          // Home
+
+          BottomNavigationBarItem(
+            icon: Container(
+              width: bottomBarWidth,
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: _page == 0
+                        ? GlobalVariable.selectedNavBarColor
+                        : GlobalVariable.backgroundColor,
+                    width: bottomBarBorderWidth,
+                  ),
+                ),
+              ),
+              child: const Icon(
+                Icons.home_outlined,
+              ),
+            ),
+            label: '',
+          ),
+
+          // Profile
+
+          BottomNavigationBarItem(
+            icon: Container(
+              width: bottomBarWidth,
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: _page == 1
+                        ? GlobalVariable.selectedNavBarColor
+                        : GlobalVariable.backgroundColor,
+                    width: bottomBarBorderWidth,
+                  ),
+                ),
+              ),
+              child: const Icon(
+                Icons.person_outline_outlined,
+              ),
+            ),
+            label: '',
+          ),
+
+          // Cart
+
+          BottomNavigationBarItem(
+            icon: Container(
+              width: bottomBarWidth,
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: _page == 2
+                        ? GlobalVariable.selectedNavBarColor
+                        : GlobalVariable.backgroundColor,
+                    width: bottomBarBorderWidth,
+                  ),
+                ),
+              ),
+              child: const Icon(
+                Icons.shopping_cart_checkout_outlined,
+              ),
+            ),
+            label: '',
+          ),
+        ],
+      ),
+    );
+  }
+}
