9fe5883b7af1699f574fc64b2e83daa9dcb2d06b
Himasnhu-AT <aristech.official@gmail.com>
2023-08-08 01:26:03 +0530

    test code, update Release Fixes #3, remove pixel overflow

--- lib/screen/splashscreen/splashscreen2.dart
+++ lib/screen/splashscreen/splashscreen2.dart
@@ -10,88 +10,91 @@ class SplashScreen2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const SizedBox(
-            height: 20,
-          ),
-          // Top of the screen, small word "Skip" button
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Align(
-              alignment: Alignment.topRight,
-              child: TextButton(
-                onPressed: () {
-                  // Handle Skip button press here
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const SignUpPage(),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            // Top of the screen, small word "Skip" button
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Align(
+                alignment: Alignment.topRight,
+                child: TextButton(
+                  onPressed: () {
+                    // Handle Skip button press here
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SignUpPage(),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    'Skip',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
                     ),
-                  );
-                },
-                child: const Text(
-                  'Skip',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
                   ),
                 ),
               ),
             ),
-          ),
-          // Image in the middle, taking complete width
-          const SizedBox(
-            height: 20,
-          ),
-          Image.asset(
-            GlobalVariable.splash2,
-            fit: BoxFit.cover,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
+            // Image in the middle, taking complete width
+            const SizedBox(
+              height: 20,
+            ),
+            Image.asset(
+              GlobalVariable.splash2,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
 
-          // Text in the middle section
-          const Expanded(
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Connect with and seek guidance from seniors of your college for a thriving and amazing career ahead of you',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
+            // Text in the middle section
+            const Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Connect with and seek guidance from seniors of your college for a thriving and amazing career ahead of you',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          // Continue button at the bottom
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: CustomButtonSplash(
-              text: 'Next Page',
-              onTap: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const SplashScreen3(),
-                  ),
-                );
-              },
+            // Continue button at the bottom
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: CustomButtonSplash(
+                text: 'Next Page',
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SplashScreen3(),
+                    ),
+                  );
+                },
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-        ],
+            const SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screen/splashscreen/splashscreen3.dart
+++ lib/screen/splashscreen/splashscreen3.dart
@@ -9,20 +9,77 @@ class SplashScreen3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const SizedBox(
-            height: 20,
-          ),
-          // Top of the screen, small word "Skip" button
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Align(
-              alignment: Alignment.topRight,
-              child: TextButton(
-                onPressed: () {
-                  // Handle Skip button press here
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            // Top of the screen, small word "Skip" button
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Align(
+                alignment: Alignment.topRight,
+                child: TextButton(
+                  onPressed: () {
+                    // Handle Skip button press here
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SignUpPage(),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    'Skip',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            // Image in the middle, taking complete width
+            const SizedBox(
+              height: 20,
+            ),
+            Image.asset(
+              GlobalVariable.splash3,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+
+            // Text in the middle section
+            const Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'CODE CREATE CARE \nWant to learn to code? For now this application is all for coding only :)',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            // Continue button at the bottom
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: CustomButtonSplash(
+                text: 'Sign UP',
+                onTap: () {
                   Navigator.pushReplacement(
                     context,
                     MaterialPageRoute(
@@ -30,67 +87,13 @@ class SplashScreen3 extends StatelessWidget {
                     ),
                   );
                 },
-                child: const Text(
-                  'Skip',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
               ),
             ),
-          ),
-          // Image in the middle, taking complete width
-          const SizedBox(
-            height: 20,
-          ),
-          Image.asset(
-            GlobalVariable.splash3,
-            fit: BoxFit.cover,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-
-          // Text in the middle section
-          const Expanded(
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'CODE CREATE CARE \nWant to learn to code? For now this application is all for coding only :)',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
+            const SizedBox(
+              height: 20,
             ),
-          ),
-          // Continue button at the bottom
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: CustomButtonSplash(
-              text: 'Sign UP',
-              onTap: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const SignUpPage(),
-                  ),
-                );
-              },
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
