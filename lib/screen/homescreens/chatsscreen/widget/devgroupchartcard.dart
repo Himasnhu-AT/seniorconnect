71703aade74a34ed197b863d0111b3c3d0beaceb
Himasnhu-AT <aristech.official@gmail.com>
2023-08-09 01:16:40 +0530

    remove minor problem

--- lib/screen/homescreens/chatsscreen/widget/devgroupchartcard.dart
+++ lib/screen/homescreens/chatsscreen/widget/devgroupchartcard.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: sized_box_for_whitespace
+
 import 'package:flutter/material.dart';
 import 'package:seniorconnect/screen/homescreens/chatsscreen/screenaddons/groupchatscreen.dart';
 
