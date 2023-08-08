05a67716c9aa7367644c04916c81a514238c68a5
Himasnhu-AT <aristech.official@gmail.com>
2023-08-09 01:03:04 +0530

    milestone 2

--- lib/screen/popupscreen/postviewscreen.dart
+++ lib/screen/popupscreen/postviewscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:seniorconnect/constants/widget/text_feild_sign.dart';
 
 import '../../constants/global_variables.dart';
 
@@ -13,6 +14,7 @@ class _PostScreenPopUpState extends State<PostScreenPopUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 0, 0, 17),
       // things to have:
       appBar: AppBar(
         iconTheme: const IconThemeData(color: Colors.white),
@@ -26,9 +28,65 @@ class _PostScreenPopUpState extends State<PostScreenPopUp> {
           ),
         ),
       ),
-      // Row1: upload image dialog. gets image from system
-      // Row2: Caption textfield
-      // Row3: Post button
+
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          children: [
+            // Row1: upload image dialog. gets image from system
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              'Upload Image',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+                color: Colors.white,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 5.0,
+              ),
+              child: Container(
+                height: 300,
+                width: double.infinity,
+                color: GlobalVariable.lightbackgroundColor,
+                child: const Center(
+                  child: Text(
+                    'Upload Image',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            // Row2: Caption textfield
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              'Caption',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w700,
+                color: Colors.white,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: 5.0,
+              ),
+              child: CustomTextFieldSign(hintText: 'Write Here'),
+            )
+            // Row3: Post button
+          ],
+        ),
+      ),
     );
   }
 }
